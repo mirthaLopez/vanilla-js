727d4d0558c761cd2ac1d1b7064e91e9dfc1714b
mirthaLopez <melopez@fwdcostarica.com>
2024-08-14 11:14:13 -0600

    class

--- src/ejercicio3.js
+++ src/ejercicio3.js
@@ -1,5 +1,38 @@
 // Inserte el código aquí
+let arreglo=[1,2,3,4];
+clonarArreglo(arreglo);
 
+function clonarArreglo (arreglo) {
+    const nuevoArreglo=[...arreglo];
+    return nuevoArreglo;
+}
+
+const perro={
+    name:"choco",
+    age:3,
+    breed:"american",
+}
+function clonarObjeto(perro) {
+    const perroClon={...perro}
+    return perroClon;
+}
+
+
+
+
+const result=sumar(1,2,3,4,5);
+
+function sumar(...numeros) {
+    return numeros.reduce((acumulador, valorActual)=>{
+        return acumulador+valorActual
+    },0)
+}
+
+
+function saludar(nombre="Desconocido") {
+    let saludo="Hola";
+    return saludo, nombre;
+}
 
 
 // NO MODIFICAR
